60435808ef8313f1295a9e080d70bfdb57c6808d
AakashMaharjan929 <aakashmaharjan2222@gmail.com>
2026-02-02 16:15:21 +0545

    Static methods, Class methods, dunder functions, private variables

--- OOP/ClassAndObject.py
+++ OOP/ClassAndObject.py
@@ -79,5 +79,72 @@ my_car.display_info()
 
 
 
+# Private attribute example
+
+class Account:
+    def __init__(self, acc, password):
+        self.account_number = acc      # private attribute
+        self.__password = password
+
+    def reset_password(self, new_password):
+        print(self.__password)
+
+acc1 = Account("123456789", "securepassword")
+print(acc1.account_number)
+print(acc1.reset_password("newpassword"))
+
+# Class method
+
+class Circle:
+    pi = 3.14
+
+    def get_pi(self):
+        return self.pi
+
+    @classmethod
+    def area(cls, radius,pi):
+        cls.pi=pi
+        return cls.pi * radius * radius
+    
+    
+    
+circle_area = Circle.area(5, 3.141)
+print("Area of circle:", circle_area)
+print("Value of pi:", Circle.get_pi(Circle))
+
+# property decorator
+
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def area(self):
+        return self._width * self._height
+
+rect = Rectangle(4, 5)
+print("Area of rectangle:", rect.area)
+
+# Polymorphism operator overloading
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def display(self):
+        print(f"Point({self.x}, {self.y})")
+
+point1 = Point(2, 3)
+point2 = Point(4, 5)
+point3 = point1 + point2
+point3.display()
+
+
+
 
 
